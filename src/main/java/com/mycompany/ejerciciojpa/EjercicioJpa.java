37d7cdfd97d54f86e418fb36a336773583dfa5a4
Oscar <oscarfranciscomaciel@gmail.com>
2025-01-07 17:54:50 -0300

    Metodo traer informacion Alumno

--- src/main/java/com/mycompany/ejerciciojpa/EjercicioJpa.java
+++ src/main/java/com/mycompany/ejerciciojpa/EjercicioJpa.java
@@ -27,7 +27,16 @@ public class EjercicioJpa {
         //llamamos al metodo para editar un alumno en la base de datos
         control.editarAlumno(alu);
         
+        //Si tenemos que buscar un alumno para ver su info podemos usar el metodo
+        Alumno alu2=control.traerAlumno(12);
+        System.out.println("EL alumno es: "+alu2.toString());
         
+
+        //Si no conocemos el id del alumno que queremos editar podemos trar a todos
+        ArrayList<Alumno> listaAlumnos=control.traerListaAlumnos();
+        for(Alumno al:listaAlumnos){
+            System.out.println("El alumno es: "+al.toString());
+        }
         
         //llamamos al metodo para eliminar un alumno de la base de datos
         control.eliminarAlumno(12);
